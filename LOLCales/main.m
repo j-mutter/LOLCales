82dc3d8f59f04a04b0f3feff11d24d35a0122f86
Justin Mutter <mutter.justin@gmail.com>
2013-09-30 13:17:47 -0400

    NSLog all available locales, purge unneeded files.

--- LOLCales/main.m
+++ LOLCales/main.m
@@ -3,16 +3,12 @@
 //  LOLCales
 //
 //  Created by Justin Mutter on 2013-09-30.
-//  Copyright (c) 2013 Jadedpixel. All rights reserved.
+//  Copyright (c) 2013 Justinc. All rights reserved.
 //
-
-#import <UIKit/UIKit.h>
-
-#import "SHPAppDelegate.h"
+#import <Foundation/Foundation.h>
 
 int main(int argc, char * argv[])
 {
-	@autoreleasepool {
-	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([SHPAppDelegate class]));
-	}
+	NSLog(@"%@", [NSLocale availableLocaleIdentifiers]);
+	return 0;
 }
