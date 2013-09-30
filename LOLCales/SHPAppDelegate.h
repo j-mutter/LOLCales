5dc0e36fc104e2b8eccb3b9fefec21b28953e1a4
Justin Mutter <mutter.justin@gmail.com>
2013-09-30 12:21:41 -0400

    Initial Commit

---
+++ LOLCales/SHPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SHPAppDelegate.h
+//  LOLCales
+//
+//  Created by Justin Mutter on 2013-09-30.
+//  Copyright (c) 2013 Jadedpixel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LOLCales/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LOLCales
+//
+//  Created by Justin Mutter on 2013-09-30.
+//  Copyright (c) 2013 Jadedpixel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SHPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([SHPAppDelegate class]));
+	}
+}
